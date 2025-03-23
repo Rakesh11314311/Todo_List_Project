ee05dbc45cb848cec2547ff03bb9d8ca1ad137fe
Rakesh11314311 <rakesh11314311@gmail.com>
2025-03-23 23:21:00 +0530

    Added delete_task function

--- todo.py
+++ todo.py
@@ -12,6 +12,13 @@ def list_tasks():
         for idx, task in enumerate(tasks, start=1):
             print(f'{idx}. {task}')
 
+def delete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        removed_task = tasks.pop(task_index)
+        print(f'Task "{removed_task}" removed.')
+    else:
+        print("Invalid task number.")
+
 
 if __name__ == "__main__":
     add_task("Finish Assignment")
